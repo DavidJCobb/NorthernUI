48865236e94b7b26fbc7de4232a1edf9ed5a9e8e
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2019-01-10 12:39:50 +0100

    Add files via upload

--- obse_plugin_example/Miscellaneous/math.h
+++ obse_plugin_example/Miscellaneous/math.h
@@ -1,10 +1,23 @@
 #pragma once
+#include <type_traits>
 
 namespace cobb {
    constexpr float pi = 3.1415926535897932384626433F;
 
    float radians_to_degrees(float rad);
    float degrees_to_radians(float deg);
+
+   //           T clamp(T value, U lo, V hi);
+   // constexpr T clamp(T value, U lo, V hi);
+   //
+   template<typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
+   inline T clamp(T value, T lo, T hi) {
+      return (value < lo) ? lo : ((value > hi) ? hi : value);
+   }
+   template<typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
+   constexpr const T& clamp(T& value, T& lo, T& hi) {
+      return (value < lo) ? lo : ((value > hi) ? hi : value);
+   }
 
    template<typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
    inline T distance(T x, T y) {
