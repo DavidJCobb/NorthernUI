39ca84eea95fcf1728ed7a2867ec16cd0d6c16e5
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2019-08-14 18:59:13 +0200

    Fixed debugging for cyclical references in menu XML
    Our debugger for tile trait cyclical references is no longer capable of producing multiple gigabytes of irrelevant garbage; it has been rewritten in full and logs only the information necessary to fully identify the cyclical reference's origin. As this information can still be fairly verbose (with 58KB log files produced by the situation that previously produced 2.5GB log files), the logging will remain turned off by default.

--- obse_plugin_example/Services/TileDump.cpp
+++ obse_plugin_example/Services/TileDump.cpp
@@ -116,98 +116,179 @@ void TileDump(RE::Tile* tile) {
    _MESSAGE(" - Tile logged.");
 }
 
-void TileCyclicalReferenceChecker::Check(RE::Tile::Value* base, bool isRecursing) {
-   if (!isRecursing) {
-      _MESSAGE("=========================================================");
-      _MESSAGE("Running cyclical reference check for %s::%s...", base->owner->name.m_data, RE::TagIDToName(base->id));
-   } else {
-      _MESSAGE("Inbound reference from %s::%s...", base->owner->name.m_data, RE::TagIDToName(base->id));
+void TileCyclicalReferenceChecker::Log(const char* format, ...) {
+   if (this->lineCount >= ce_safeMaxLines)
+      return;
+   va_list args;
+   va_start(args, format);
+   std::string formatPrefix;
+   formatPrefix += format;
+   {  // Some of Bethesda's format messages are wrong.
+      std::size_t found = formatPrefix.find("%S", 20);
+      while (found != std::string::npos) {
+         formatPrefix[found + 1] = 's';
+         found = formatPrefix.find("%S", found);
+      }
    }
+   gLog.Log(IDebugLog::kLevel_Message, formatPrefix.c_str(), args);
+   va_end(args);
+   this->lineCount++;
+   if (this->lineCount >= ce_safeMaxLines) {
+      _MESSAGE("-- HIT MAX LOG LINE THRESHOLD FOR THIS CHECK; WILL ABORT SOON --");
+   }
+}
+bool TileCyclicalReferenceChecker::IsLoggingSafe() const {
+   return this->lineCount < ce_safeMaxLines;
+}
+RE::Tile::Value* TileCyclicalReferenceChecker::FindCyclical(RE::Tile::Value* base, bool isRecursing) {
    for (auto it = this->found.begin(); it != this->found.end(); ++it) {
-      if (*it == base) {
-         _MESSAGE("!!! !!! !!! CYCLICAL REFERENCE DETECTED !!! !!! !!!");
-         _MESSAGE("%s::%s has been seen before!", base->owner->name.m_data, RE::TagIDToName(base->id));
-         _MESSAGE("");
-         return;
-      }
+      if (*it == base)
+         return base;
    }
-   //
-   // Mimic Tile::Value::DoActionEnumeration(bool):
-   //
    this->found.push_back(base);
-   auto current = base->operators->next;
-   _MESSAGE("Operators:");
-   if (!base->operators->next)
-      _MESSAGE(" - <None>");
-   for (auto current = base->operators->next; current; current = current->next) {
-      auto name = RE::TagIDToName(current->opcode);
-      switch (current->opcode) {
-         case 0xA:
-            _MESSAGE(" - %08X <%s>...", current, name);
-            break;
-         case 0xF:
-            _MESSAGE(" - %08X ...</%s>", current, name);
-            break;
-         case 0x65:
-            _MESSAGE(" - %08X List Head", current);
-            break;
-         default:
-            if (current->opcode == 0)
-               name = "[NEUTRALIZED]";
-            if (name) {
-               if (current->refPrev) {
-                  auto p = current->refPrev;
-                  while (p->prev)
-                     p = p->prev;
-                  if (p->opcode == 0x65) {
-                     auto source = p->operand.ref;
-                     if (source) {
-                        auto tile  = source->owner->name.m_data;
-                        auto trait = RE::TagIDToName(source->id);
-                        if (trait)
-                           _MESSAGE(" - %08X <%s src=\"%s\" trait=\"%s\" /> == %f", current, name, tile, trait, current->operand.immediate);
-                        else
-                           _MESSAGE(" - %08X <%s src=\"%s\" trait=\"0x%08X\" /> == %f", current, name, tile, source->id, current->operand.immediate);
-                     } else
-                        _MESSAGE(" - %08X <%s src=\"?\" trait=\"?\" /> == %f", current, name, current->operand.immediate);
-                  } else {
-                     _MESSAGE(" - %08X <%s src=\"?\" trait=\"?\" /> == %f", current, name, current->operand.immediate);
-                  }
-               } else {
-                  _MESSAGE(" - %08X <%s>%f</%s>", current, name, current->operand.immediate, name);
-               }
-            } else {
-               _MESSAGE(" - %08X with opcode %03X", current, current->opcode);
-            }
-      }
-   }
-   _MESSAGE("");
    //
    // Mimic Tile::Value::UpdateInboundReferences():
    //
    RE::Tile::Value* lastIncomingValue = nullptr;
    for (auto incoming = base->incomingRefs->refNext; incoming; incoming = incoming->refNext) {
-      if (incoming->opcode == 0) {
-         _MESSAGE("    - Found dead opcode %08X with operand %08X.", incoming, incoming->operand.opcode);
+      if (!this->IsLoggingSafe())
+         return nullptr;
+      if (incoming->opcode == 0)
          continue;
-      }
       RE::Tile::Value::Expression* rel = incoming;
       while (rel->prev)
          rel = rel->prev;
       auto relValue = rel->operand.ref;
       if (!relValue || relValue == lastIncomingValue)
          continue;
-      this->Check(relValue, true);
-      _MESSAGE(" = Returning to %s::%s inbound references...\n", base->owner->name.m_data, RE::TagIDToName(base->id));
+      auto cyclical = this->FindCyclical(relValue, true);
+      if (cyclical)
+         return cyclical;
       lastIncomingValue = relValue;
    }
    //
    // Done.
    //
    this->found.pop_back();
+   return nullptr;
+}
+void TileCyclicalReferenceChecker::Check(RE::Tile::Value* base, bool isRecursing) {
    if (!isRecursing) {
-      _MESSAGE("Check complete. If no circular reference was listed, then none were found.");
+      this->indent = "";
       _MESSAGE("=========================================================");
+      _MESSAGE("Running cyclical reference check for %s::%s...", base->owner->name.m_data, RE::TagIDToName(base->id));
+      auto cyclical = this->FindCyclical(base);
+      if (!cyclical) {
+         _MESSAGE("No cyclical reference found.");
+         _MESSAGE("=========================================================");
+         return;
+      }
+      _MESSAGE("Found cyclical reference! Stack:");
+      for (auto it = this->found.begin(); it != this->found.end(); ++it) {
+         auto value = *it;
+         if (value == cyclical)
+            _MESSAGE(" > %s::%s", value->owner->name.m_data, RE::TagIDToName(value->id));
+         else
+            _MESSAGE(" - %s::%s", value->owner->name.m_data, RE::TagIDToName(value->id));
+      }
+      _MESSAGE("");
+      _MESSAGE("Tracing reference chain from end to start...");
+      _MESSAGE("NOTE: Srcs shown on operators represent the final resolved value: selectors \n"
+               "like me() and sibling() will be replaced by the final resolved tile's name.");
+      _MESSAGE("");
+   }
+   auto count = this->found.size();
+   if (count) {
+      RE::Tile::Value* value    = *this->found.rbegin();
+      RE::Tile::Value* previous = nullptr;
+      if (count > 1)
+         previous = *(this->found.rbegin() + 1);
+      this->found.pop_back();
+      auto prefix = this->indent.c_str();
+      if (isRecursing)
+         _MESSAGE("%s - Outbound reference to value %s::%s, whose operators are:", prefix, value->owner->name.m_data, RE::TagIDToName(value->id));
+      else
+         _MESSAGE("%s > Circular reference to value %s::%s, whose operators are:", prefix, value->owner->name.m_data, RE::TagIDToName(value->id));
+      auto op = value->operators->next;
+      if (op) {
+         bool loggedRelevant = false;
+         do {
+            if (loggedRelevant && isRecursing) {
+               _MESSAGE("%s    + Remaining operators for this tile are irrelevant and will not be logged.", prefix);
+               break;
+            }
+            bool isPotentiallyRelevant = false;
+            //
+            RE::Tile::Value* sourceVal = nullptr;
+            RE::Tile::Value::Expression* sourceHead;
+            for (sourceHead = op->refPrev; sourceHead; sourceHead = sourceHead->refPrev) {
+               if (sourceHead->opcode == 0x65)
+                  break;
+            }
+            if (sourceHead) {
+               sourceVal = sourceHead->operand.ref;
+               if (sourceVal)
+                  isPotentiallyRelevant = sourceVal == previous || !previous;
+            }
+            char bullet = isPotentiallyRelevant ? '-' : '+';
+            auto name = RE::TagIDToName(op->opcode);
+            switch (op->opcode) {
+               case 0xA:
+                  _MESSAGE("%s    %c %08X <%s>...", prefix, bullet, op, name);
+                  break;
+               case 0xF:
+                  _MESSAGE("%s    %c %08X ...</%s>", prefix, bullet, op, name);
+                  break;
+               case 0x65:
+                  _MESSAGE("%s    %c %08X List Head", prefix, bullet, op);
+                  break;
+               default:
+                  if (op->opcode == 0)
+                     name = "[NEUTRALIZED]";
+                  if (name) {
+                     if (op->refPrev) {
+                        auto p = op->refPrev;
+                        while (p->prev)
+                           p = p->prev;
+                        if (p->opcode == 0x65) {
+                           auto source = p->operand.ref;
+                           if (source) {
+                              auto tile  = source->owner->name.m_data;
+                              auto trait = RE::TagIDToName(source->id);
+                              if (trait)
+                                 _MESSAGE("%s    %c %08X <%s src=\"%s\" trait=\"%s\" /> == %f", prefix, bullet, op, name, tile, trait, op->operand.immediate);
+                              else
+                                 _MESSAGE("%s    %c %08X <%s src=\"%s\" trait=\"0x%08X\" /> == %f", prefix, bullet, op, name, tile, source->id, op->operand.immediate);
+                           } else
+                              _MESSAGE("%s    %c %08X <%s src=\"?\" trait=\"?\" /> == %f", prefix, bullet, op, name, op->operand.immediate);
+                        } else {
+                           _MESSAGE("%s    %c %08X <%s src=\"?\" trait=\"?\" /> == %f", prefix, bullet, op, name, op->operand.immediate);
+                        }
+                     } else {
+                        _MESSAGE("%s    %c %08X <%s>%f</%s>", prefix, bullet, op, name, op->operand.immediate, name);
+                     }
+                  } else {
+                     _MESSAGE("%s    %c %08X with opcode %03X", prefix, bullet, op, op->opcode);
+                  }
+            }
+            if (isPotentiallyRelevant) {
+               if (previous) {
+                  auto size = this->indent.size();
+                  this->indent.resize(size + 6, ' ');
+                  this->Check(previous, true);
+                  this->indent.resize(size);
+                  prefix = this->indent.c_str();
+               } else {
+                  _MESSAGE("%s       > Circular reference to the outermost-logged value.", prefix);
+               }
+               loggedRelevant = true;
+            }
+         } while (op = op->next);
+      }
+   }
+   if (!isRecursing) {
       this->found.clear();
+      _MESSAGE("\nTrace complete.");
+      _MESSAGE("=========================================================");
    }
 }
--- obse_plugin_example/Services/TileDump.h
+++ obse_plugin_example/Services/TileDump.h
@@ -4,9 +4,17 @@
 void TileDump(RE::Tile*);
 
 struct TileCyclicalReferenceChecker {
+   static constexpr UInt32 ce_safeMaxLines = 500;
+
    std::vector<RE::Tile::Value*> found;
+   std::string indent;
+   UInt32 lineCount;
    //
+   bool IsLoggingSafe() const;
+   void Log(const char* format, ...);
    void Check(RE::Tile::Value*, bool isRecursing = false);
+
+   RE::Tile::Value* FindCyclical(RE::Tile::Value*, bool isRecursing = false);
 
    static TileCyclicalReferenceChecker& GetInstance() {
       static TileCyclicalReferenceChecker instance;
